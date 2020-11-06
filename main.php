b0aaf13fe26448e6f223995d9b14a041fb2930e8
SrideviKrishnakumar <71437326+SrideviKrishnakumar@users.noreply.github.com>
2020-11-06 16:38:32 +0100

    Update main.php

--- main.php
+++ main.php
@@ -10,24 +10,29 @@ require_once("nav.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="styles/style.css">
 </head>
 
 <body>
+    <main>
 
-    <section>
-        <article>
-            <h1> TIME PASS</h1>
+        <section>
+            <article>
+                <h1> TIME PASS</h1>
 
-            <p> TimePass Inc. is an NumericAll technology and media services provider and production company headquartered in Luxembourg.
-                TimePass was founded in 2020 by Emile and Sridevi. The company's primary business is its
-                subscription-based streaming service which offers online streaming of a library of films and television series, including those produced in-house. As of Nov 2020, TimePass had over 193 million paid subscriptions worldwide, including 73 million in the United States. It is available worldwide except in the following: mainland China, Syria, North Korea, and Crimea. The company also has offices in France, the United Kingdom, Brazil, the Netherlands, India, Japan, and South Korea.
-                TimePass is a member of the Motion Picture Association. Today, the company produces and distributes content from countries all over the globe.</p>
-        </article>
-        <article>
-            <input type="text" name="search" id='#search' value="SEARCH" class="size">
-        </article>
-    </section>
+                <p> TimePass Inc. is an NumericAll technology and media services provider and production company headquartered in Luxembourg.
+                    TimePass was founded in 2020 by Emile and Sridevi. The company's primary business is its
+                    subscription-based streaming service which offers online streaming of a library of films and television series, including those produced in-house. As of Nov 2020, TimePass had over 193 million paid subscriptions worldwide, including 73 million in the United States. It is available worldwide except in the following: mainland China, Syria, North Korea, and Crimea. The company also has offices in France, the United Kingdom, Brazil, the Netherlands, India, Japan, and South Korea.
+                    TimePass is a member of the Motion Picture Association. Today, the company produces and distributes content from countries all over the globe.</p>
+            </article>
+            <article id='list'>
+                <input type="text" name="search" id='search' value="SEARCH" class="size">
+            </article>
+            <div id='result'></div>
+
+        </section>
+    </main>
 
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous">
     </script>
@@ -44,6 +49,7 @@ require_once("nav.php");
                     data: {
                         moviename: $('#search').val() // can be used as $(this)
                     }
+
                 }).done(function(result) {
                     // If ajax call worked
                     console.log('SUCCESS : ' + result);
@@ -57,6 +63,7 @@ require_once("nav.php");
 
         });
     </script>
+
 </body>
 
 </html>
