cb3148ac630e52c622961645e23c2d9935ad2df8
SrideviKrishnakumar <71437326+SrideviKrishnakumar@users.noreply.github.com>
2020-11-06 14:44:49 +0100

    Update nav.php

---
+++ nav.php
@@ -0,0 +1,36 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+
+<body>
+    <nav>
+        <ul>
+            <li>
+                <a href="#home.php" class="box">HOME </a>
+                <a href="#category.php" class="box">CATEGORIES </a>
+                <a href="insert.php" class='not_visible'>ADD_MOVIES </a>
+            </li>
+            <li>
+                <a href="" class="box">Login</a>
+                <a href="" class="box">Register</a>
+                <a href="" class='not_visible'>Logout</a>
+
+            </li>
+
+        </ul>
+        <hr>
+        <hr>
+    </nav>
+
+</body>
+
+</html>
