cc3eef1f752b18fcb85e958b9e7a50217e83910e
Emile-DiDo <71436822+Emile-DiDo@users.noreply.github.com>
2020-11-06 16:38:16 +0100

    Update categories.php

--- categories.php
+++ categories.php
@@ -1,5 +1,7 @@
 <?php
 require_once "nav.php";
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,7 +10,22 @@ require_once "nav.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="styles/categories.css">
 </head>
+<header>
+    <form action="" method="post">
+        <select name="categories" id="cat">
+            <option value="%">All Categories</option>
+        </select>
+        <input type="submit" value="Search with specifications">
+        <select name="" id="">
+            <option value="ASC"> Order by Ascending</option>
+            <option value="DESC">Order by Descending</option>
+        </select>
+    </form>
+
+</header>
 
 <body>
 
