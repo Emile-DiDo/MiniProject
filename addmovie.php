f7d57c86ff9100e248c6684379330693f6122592
SrideviKrishnakumar <71437326+SrideviKrishnakumar@users.noreply.github.com>
2020-11-09 16:17:41 +0100

    Create addmovie.php

---
+++ addmovie.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>ADD MOVIE</h1>
+    <br>
+    <form action="" method="post">
+        <label>Title</label>
+        <input type="text">
+        <br>
+        <label>Release Year</label>
+        <input type="text">
+        <br>
+        <label>Description</label>
+        <input type="textarea">
+        <br>
+        <label for="">PosterPath</label>
+        <input type="text">
+        <br>
+        <label for="">Category</label>
+        <input type="text" id='category'>
+        <input type="submit">Append the movie
+
+
+    </form>
+
+</body>
+
+</html>
