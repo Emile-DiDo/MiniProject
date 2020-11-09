258c831ab59864dcb2d49e9acf2c799a2ffdecd8
Emile-DiDo <71436822+Emile-DiDo@users.noreply.github.com>
2020-11-09 13:36:52 +0100

    Update ordercat.js

---
+++ scripts/ordercat.js
@@ -0,0 +1,98 @@
+$(function () {
+            
+    $('#ordercat').click(function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: 'ordercat.php',
+            method: 'get',
+            data:{
+                cat: $("#cat").val(),
+                sort:$("#sort").val(),
+            },
+            dataType: 'json'
+        }).done(function(result) {
+            let movies = result;
+            console.log(movies);
+            $("#ordered").html("");
+            $.each(movies, function(key, movie) {
+                $('#ordered').append('<div class="clone">')
+                $('.clone').append('<div class="one" id="one">')
+                $('.clone').append('<div class="two" id="two">')
+                $('.clone').append('<div class="three" id=three>')
+                $('#one').append('<img src="' + movie.poster + '" alt="" srcset="">');
+                $('#two').append('<h2>#' + movie.id +"<a href='" + movie.id + "'>" + movie.title + '</a></h2>');
+                $('#two').append('<p>  ' + movie.descriptionCut + ' ...</p>');
+                $('#three').append('<a href="' + movie.id + '" alt="" srcset="">Modify Movie</a>');
+                $('#three').append('<a href="<?php header("Location: main.php?id='+ +'");?>' + movie.description + '" alt="" srcset="">See Details</a>');
+                $('#one').removeAttr("id");
+                $('#two').removeAttr("id");
+                $('#three').removeAttr("id");
+                $('.clone').removeClass("clone");
+                  
+                console.log('SUCCESS : ' + result);
+            });
+        }).fail(function (result) {
+            console.log('AJAX ERROR:' + result);
+            $('#movies').html(result);
+        });
+    });
+});
+$(function () {
+            
+    $('#ordered').ready(function (e) {
+
+        $.ajax({
+            url: 'allmovies.php',
+            method: 'get',
+            dataType: 'json'
+
+        }).done(function(result) {
+            let movies = result;
+            console.log(movies);
+            $("#ordered").html("");
+            $.each(movies, function(key, movie) {
+                $('#ordered').append('<div id="clone" class="clone">')
+                $('#clone').append('<div class="one" id="one">')
+                $('#clone').append('<div class="two" id="two">')
+                $('#clone').append('<div class="three" id=three>')
+                $('#one').append('<img src="' + movie.poster + '" alt="" srcset="">');
+                $('#two').append('<h2>#' + movie.id +"<a href='" + movie.id + "'>" + movie.title + '</a></h2>');
+                $('#two').append('<p>  ' + movie.descriptionCut + ' ...</p>');
+                $('#three').append('<p><a href="' + movie.id + '" alt="" srcset="">Modify Movie</a></p>');
+                $('#three').append('<p><a href="<?php header("Location: main.php?id='+ +'");?>' + movie.description + '" alt="" srcset="">See Details</a></p>');
+                $('#one').removeAttr("id");
+                $('#two').removeAttr("id");
+                $('#three').removeAttr("id");
+                $('#clone').removeAttr("id");
+                console.log('SUCCESS : ' + result);
+            });
+
+        }).fail(function (result) {
+            console.log('AJAX ERROR:' + result);
+            $('#ordered').html(result);
+        });
+    });
+})
+$(function () {
+            
+    $('#cat').ready(function (e) {
+
+        $.ajax({
+            url: 'allcat.php',
+            method: 'get',
+            dataType: 'json'
+
+        }).done(function(result) {
+            let cat = result;
+            console.log(cat);
+            $.each(cat, function(key, cat) {
+                $('#cat').append('<option value="' + cat.id + '">'+ cat.name +'</option>');
+                console.log('SUCCESS : ' + result);
+            });
+
+        }).fail(function (result) {
+            console.log('AJAX ERROR:' + result);
+            $('#ordered').html(result);
+        });
+    });
+})
