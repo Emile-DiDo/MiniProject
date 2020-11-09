b2b5bc829bc9a627fa0c81d02b716b8cdbde867a
Emile-DiDo <71436822+Emile-DiDo@users.noreply.github.com>
2020-11-09 14:06:55 +0100

    Update ordercat.php

--- ordercat.php
+++ ordercat.php
@@ -1,8 +1,11 @@
 <?php
-require_once "databas.php";
+require_once "database.php";
 $cat = trim($_GET["cat"]);
 $sort = trim($_GET["sort"]);
-$query = "SELECT SUBSTRING(description, 1, 30) as descriptionCut, movies.id, title, poster FROM movies inner Join categories on categories.id= movies.category_id where categories.id like '$order' ORDER by title $sort ";
+$query = "SELECT SUBSTRING(description, 1, 30)
+ as descriptionCut, movies.id, title, poster FROM
+  movies inner Join categories on categories.id= movies.category_id
+   where categories.id like '$cat' ORDER by title $sort ";
 $result = mysqli_query($conn, $query);
 $movies = mysqli_fetch_all($result, MYSQLI_ASSOC);
 $object = json_encode($movies, JSON_PRETTY_PRINT);
