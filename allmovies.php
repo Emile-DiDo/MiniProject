4582773607dee0d97dee0f82080820aa2b90f52d
Emile-DiDo <71436822+Emile-DiDo@users.noreply.github.com>
2020-11-09 09:42:58 +0100

    Create allmovies.php

---
+++ allmovies.php
@@ -0,0 +1,7 @@
+<?php
+require_once "databas.php";
+$query = "SELECT * FROM movies";
+$result = mysqli_query($conn, $query);
+$movies = mysqli_fetch_all($result, MYSQLI_ASSOC);
+$object = json_encode($movies, JSON_PRETTY_PRINT);
+echo $object;
