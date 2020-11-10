e26e7d6084fc6a984b7c7fc98a228d57d80205f5
Emile-DiDo <71436822+Emile-DiDo@users.noreply.github.com>
2020-11-10 16:30:33 +0100

    Update modifycatquery.php

--- modifycatquery.php
+++ modifycatquery.php
@@ -8,11 +8,19 @@ echo $movies;*/
 $id = $_POST["id"];
 $name = $_POST["name"];
 if ($_POST["id"] > 0) {
-    $query = "UPDATE categories SET name =$name WHERE id =$id";
+    $query = 'UPDATE categories SET name ="' . $name . '" WHERE id =' . $id;
     $result = mysqli_query($conn, $query);
-    echo $result;
+    if ($result) {
+        echo '<p>Succsessfully modified</p>';
+    } else {
+        echo '<p>Something went wrong while modifying</p>';
+    }
 } else {
-    $query = "INSERT INTO categories(name) VALUES ($name)";
+    $query = 'INSERT INTO categories(name) VALUES ("' . $name . '")';
     $result = mysqli_query($conn, $query);
-    echo $result;
+    if ($result) {
+        echo '<p>Succsessfully added</p>';
+    } else {
+        echo '<p>Something went wrong with adding</p>';
+    }
 }
