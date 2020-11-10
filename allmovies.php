8c9624ea1eb8c5af819d98a985166f9f93731e9b
Emile-DiDo <71436822+Emile-DiDo@users.noreply.github.com>
2020-11-10 14:47:41 +0100

    Update allmovies.php

--- allmovies.php
+++ allmovies.php
@@ -1,6 +1,7 @@
 <?php
 require_once "database.php";
-$query = "SELECT SUBSTRING(description, 1, 30) as descriptionCut, id, title, poster FROM movies";
+$query = "SELECT SUBSTRING(description, 1, 30)
+ as descriptionCut, id, title, poster FROM movies limit 0,2";
 $result = mysqli_query($conn, $query);
 $movies = mysqli_fetch_all($result, MYSQLI_ASSOC);
 $object = json_encode($movies, JSON_PRETTY_PRINT);
